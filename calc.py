15ac267509b2a3335f69862622f2c9a34309ea0c
yourgalgaks97 <gakiisharon2020@gmail.com>
2022-05-19 12:26:14 +0300

    strings in python

---
+++ calc.py
@@ -0,0 +1,6 @@
+f_num = input("enter the first number :")
+s_num = input("enter the second number :")
+
+sum_of_nums =int(f_num) + int(s_num)
+
+print("the sum of the two numbers is")+ str(sum_of_nums)
---
+++ strings.py
@@ -0,0 +1,54 @@
+name = "sharon"
+msg = """ My name is Bob
+    i am from Kiambu
+    i love travelling
+    Nice to meet you"""
+print(msg)
+
+
+
+city = "nairobi"
+# .upper() to convert to upper case
+print(city.upper())
+
+
+Uni = "JKUAT"
+print(Uni.lower())
+
+
+fruit = "grapes"
+print(fruit[:2])
+print(fruit[:5])
+print(fruit[3:])
+
+print(fruit[-2:])
+
+# strip() removes spaces between the word
+f_name= "Bo b Afw ata"
+print(f_name.strip())
+g_name = " sharon"
+print(g_name.strip())
+
+fr_name = "sharon "
+sn_name= "gakii"
+
+full_name = fr_name +sn_name
+print(full_name)
+
+
+city = "mombasa"
+print(len(city))
+
+word = "abracadabra"
+print(len(word))
+
+wordtwo = "deoxyrhebonucleic"
+print(len(wordtwo))
+
+# if you want to convert an integer onto a string
+x = 100
+
+print(str(x))
+
+y = 3.142 #float
+print(int(y)) #int
